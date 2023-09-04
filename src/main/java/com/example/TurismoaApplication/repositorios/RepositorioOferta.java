c1da1c911da546e1f60966641886b7a18a9a8bb9
Johan <tovonxxxx@gmail.com>
2023-09-04 16:38:51 -0500

    cambios1

---
+++ src/main/java/com/example/TurismoaApplication/repositorios/RepositorioOferta.java
@@ -0,0 +1,8 @@
+package com.example.TurismoaApplication.repositorios;
+
+import com.example.TurismoaApplication.modelos.Oferta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepositorioOferta extends JpaRepository<Oferta, Integer> {
+
+}
---
+++ src/main/java/com/example/TurismoaApplication/servicios/OfertaServicio.java
@@ -0,0 +1,84 @@
+package com.example.TurismoaApplication.servicios;
+
+import com.example.TurismoaApplication.modelos.Empresa;
+import com.example.TurismoaApplication.modelos.Oferta;
+import com.example.TurismoaApplication.repositorios.RepositorioOferta;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class OfertaServicio {
+    @Autowired
+    RepositorioOferta repositorioOferta;
+
+
+    public Oferta registrarOferta(Oferta datosARegistrar) throws Exception {
+        try {
+            return (this.repositorioOferta.save(datosARegistrar));
+        } catch (Exception error) {
+            throw new Exception(error.getMessage());
+        }
+    }
+
+
+    public Oferta modificarOferta(Integer id, Oferta datosAModificar) throws Exception {
+        try {
+            Optional<Oferta> ofertaEncontrada = this.repositorioOferta.findById(id);
+            if (ofertaEncontrada.isEmpty()) {
+                throw new Exception("Oferta no encontrada");
+            }
+
+            Oferta ofertaExiste = ofertaEncontrada.get();
+            ofertaExiste.setTitulo(datosAModificar.getTitulo());
+            ofertaExiste.setDescripcion(datosAModificar.getDescripcion());
+
+            return (this.repositorioOferta.save(ofertaExiste));
+
+        } catch (Exception error) {
+            throw new Exception(error.getMessage());
+        }
+    }
+
+    public Oferta buscarOfertaPorId(Integer id) throws Exception {
+        try {
+            Optional<Oferta> ofertaOpcional = this.repositorioOferta.findById(id);
+            if (ofertaOpcional.isEmpty()){
+                throw new Exception("No se ha encontrado la oferta solicitada");
+            }
+            return ofertaOpcional.get();
+        }catch (Exception error){
+            throw new Exception(error.getMessage());
+        }
+    }
+
+    public List<Oferta> buscarTodasLasOfertas() throws Exception{
+        try {
+            List<Oferta>listaOfertas= this.repositorioOferta.findAll();
+            return listaOfertas;
+        }catch (Exception error){
+            throw new Exception(error.getMessage());
+        }
+    }
+
+    public Boolean eliminarOferta (Integer id) throws Exception{
+        try {
+            Optional<Oferta> ofertaOpcional = this.repositorioOferta.findById(id);
+
+            if (ofertaOpcional.isPresent()){
+                this.repositorioOferta.deleteById(id);
+                return true;
+            }else{
+                throw new Exception("Empresa no encontrada");
+            }
+
+        }catch (Exception error){
+            throw new Exception(error.getMessage());
+        }
+    }
+
+
+}
